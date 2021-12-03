f62e35a2851a4d555b1a04120ba1aa3ba82ccf8b
gbritez53 <gbritez53@gmail.com>
2021-12-03 02:27:41 -0300

    feat: Adding eslint, husky, lint-staged, prettier and NextJs basic structure

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const App = ({ Component, PageProps }) => {
+  return <Component {...PageProps} />
+}
+
+export default App
---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+      <h2>Hello Next js</h2>
+    </div>
+  )
+}
+
+export default index
