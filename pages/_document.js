9b0090bf0b005b5fb15f867dc1de49010849da32
gbritez53 <gbritez53@gmail.com>
2021-12-05 16:37:34 -0300

    Adding meta tags from social media

---
+++ pages/_document.js
@@ -0,0 +1,38 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charset="UTF-8" />
+          <meta
+            name="description"
+            content="My name is Gabriel Britez and this is my portfolio, where you can find the projects and technologies in which I have worked. "
+          />
+          <meta
+            property="og:title"
+            content="Personal Portfolio | Gabriel Britez"
+          />
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:description"
+            content="My name is Gabriel Britez and this is my portfolio, where you can find the projects and technologies in which I have worked. "
+          />
+          <meta property="og:image" content="https://i.imgur.com/HIWmajr.png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
