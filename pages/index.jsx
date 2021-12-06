a339cedad006b1957d187579451c6034a76998c1
gbritez53 <gbritez53@gmail.com>
2021-12-05 22:38:07 -0300

    fix: responsive all devices

--- pages/index.jsx
+++ pages/index.jsx
@@ -16,7 +16,7 @@ const Home = () => {
       <FullPageSections>
         <section className="bg-coolGray-100 dark:bg-coolGray-900 text-gray-900 dark:text-white px-4 md:px-20">
           <FullpageSection>
-            <section className="bg-gradient flex flex-col justify-center items-center h-full dark:text-white relative ">
+            <section className="bg-gradient flex flex-col justify-center items-center h-full dark:text-white relative pb-24">
               {systemTheme !== 'dark' ? (
                 <Image src="/logo.svg" alt="logo" width={150} height={150} />
               ) : (
@@ -52,7 +52,7 @@ const Home = () => {
                 </a>
               </div>
 
-              <div className="absolute bottom-0 flex flex-col animate-bounce items-center">
+              <div className="absolute bottom-5 md:bottom-0 flex flex-col animate-bounce items-center">
                 <p>Scroll Down</p>
                 <CgArrowDown className="text-3xl dark:text-lime-400 text-indigo-600 text-center" />
               </div>
@@ -60,9 +60,9 @@ const Home = () => {
           </FullpageSection>
 
           <FullpageSection>
-            <section className="flex flex-col items-center justify-center h-screen mx-auto">
-              <div className="flex flex-col items-center md:grid md:grid-cols-3 gap-x-12 gap-y-8">
-                <h2 className="text-3xl md:text-4xl font-bold md:col-span-3 md:col-start-2 ">
+            <section className="flex flex-col items-center justify-center h-screen mx-auto py-8">
+              <div className="flex flex-col items-center md:grid md:grid-cols-3 gap-x-12 gap-y-8 md:gap-y-4">
+                <h2 className="text-2xl md:text-4xl font-bold md:col-span-3 md:col-start-2 ">
                   About me
                 </h2>
                 <figcaption className="about-me relative w-32 md:w-52 justify-self-end">
@@ -75,17 +75,17 @@ const Home = () => {
                     className="avatar rounded-full"
                   />
                 </figcaption>
-                <div className=" w-11/12 col-span-2">
+                <div className=" w-11/12 col-span-2 text-sm md:text-base">
                   <h3 className="font-semibold text-xl">Profile:</h3>
-                  <p className="break-words dark:text-gray-300 pb-8 md:pb-0">
+                  <p className="break-words dark:text-gray-300 pb-8 md:pb-4">
                     I am enthusiastic about web application development and
                     interface design. Passionate about self-taught learning and
                     someone self-critical with me work, constantly seeking to
                     improve. I consider someone direct and responsible.
                   </p>
                   <h3 className="font-semibold text-xl">Education:</h3>
-                  <div className="grid grid-cols-2 gap-6 md:gap-4">
-                    <div className="text-gray-700 dark:text-gray-300">
+                  <div className="grid grid-cols-2 gap-4">
+                    <div className="text-gray-700 dark:text-gray-300 text-sm md:text-base">
                       <span className="text-indigo-600 dark:text-lime-300">
                         2015 - 2019
                       </span>
@@ -93,7 +93,7 @@ const Home = () => {
                         National University of Misiones. <br /> (UNAM - FCEQyN)
                       </p>
                     </div>
-                    <div className="text-gray-700 dark:text-gray-300">
+                    <div className="text-gray-700 dark:text-gray-300 text-sm md:text-base">
                       <span className="text-indigo-600 dark:text-lime-300">
                         2020 - 2022
                       </span>
